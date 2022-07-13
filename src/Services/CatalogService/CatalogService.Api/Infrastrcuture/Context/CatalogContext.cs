50db11abb7c14c2524d24e56b21c796156cd4891
Naib Residov <residovnaib77@gmail.com>
2022-07-13 12:40:02 -0700

    finsihed

--- src/Services/CatalogService/CatalogService.Api/Infrastrcuture/Context/CatalogContext.cs
+++ src/Services/CatalogService/CatalogService.Api/Infrastrcuture/Context/CatalogContext.cs
@@ -1,6 +1,30 @@
-﻿namespace CatalogService.Api.Infrastrcuture.Context
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace CatalogService.Api.Infrastrcuture.Context
 {
-    public class CatalogContext : DbContext
+    public class CatalogContext /*: DbContext*/
     {
+    }
+    public class CatalogContextSeed
+    {
+        public async Task SeedAsync(CatalogContext context, IWebHostEnvironment env, ILogger<CatalogContextSeed> seed)
+        {
+
+        }
+        private async void GetCatalogPrictures(string contentPath,string picturePath)
+        {
+            picturePath ??= "pics";
+            if (picturePath!=null)
+            {
+                DirectoryInfo info=new DirectoryInfo(picturePath);
+                foreach (var item in info.GetFiles())
+                {
+                    item.Delete();
+                }
+            }
+        }
     }
 }
