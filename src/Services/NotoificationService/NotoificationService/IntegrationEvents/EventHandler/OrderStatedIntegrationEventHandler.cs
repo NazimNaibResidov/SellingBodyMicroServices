2a5e34d7218b3872e54d9224e2e44f575feb2c52
Naib Residov <residovnaib77@gmail.com>
2022-07-19 09:42:55 -0700

    this is data

---
+++ src/Services/NotoificationService/NotoificationService/IntegrationEvents/EventHandler/OrderStatedIntegrationEventHandler.cs
@@ -0,0 +1,17 @@
+﻿using EventBus.Base.Abstrasctions;
+using Microsoft.Extensions.Logging;
+using NotoificationService.IntegrationEvents.Events;
+using System.Threading.Tasks;
+
+namespace NotoificationService.IntegrationEvents.EventHandler
+{
+    public class OrderStatedIntegrationEventHandler : IIntegrationEventHandler<OrderPaymnetFailedIntegrationEvent>
+    {
+        private readonly ILogger<PaymentService.Api.IntegrationEvents.Events.OrderStatedIntegrationEvent> logger;
+
+        public Task Handle(OrderPaymnetFailedIntegrationEvent @event)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- src/Services/NotoificationService/NotoificationService/Program.cs
+++ src/Services/NotoificationService/NotoificationService/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using EventBus.Base;
+using EventBus.Base.Abstrasctions;
+using EventBus.Factory;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
 
 namespace NotoificationService
 {
@@ -7,6 +12,27 @@ namespace NotoificationService
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            ServiceCollection service = new ServiceCollection();
+            ConfiugerService(service);
+        }
+
+        private static void ConfiugerService(ServiceCollection service)
+        {
+            service.AddLogging(config =>
+            {
+                config.AddConsole();
+            });
+            service.AddSingleton<IEventBus>(sp =>
+            {
+                EventBusConfig confog = new()
+                {
+                    ConnectionRetryCount = 5,
+                    EventNameSuffix = "EventBusTask",
+                    SubscriptionClinetAppName = "NotificationService",
+                    eventBusType = EventBusType.RabbitMq
+                };
+                return EventBusFactory.Create(confog, sp);
+            });
         }
     }
 }
